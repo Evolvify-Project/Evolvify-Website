596f8024e643341faf934e47117bb6e11a424c27
Ahmad Moemen <37182876+AhmadMoemen@users.noreply.github.com>
2025-05-14 05:05:38 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   base: "./",
   server: {
+    historyApiFallback: true,
     proxy: {
       "/Images": {
         target: "https://evolvify.runasp.net",
